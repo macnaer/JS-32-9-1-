0f49a069a8b26a5180daa4b806c1bdd5dd3c3d7e
master <master@rivne.itstep.org>
2019-12-18 09:54:04 +0200

    Simple example

--- 3. DOM/js/index.js
+++ 3. DOM/js/index.js
@@ -1,8 +1,19 @@
 window.addEventListener("load", Init);
 
+let userArr = [
+  {
+    ID: "1",
+    Name: "Name",
+    Email: "Email",
+    TelNumber: "Tel. Number",
+    Message: "Message"
+  }
+];
+
 function Init() {
   const sendMSG = document.querySelector(".sendMSG");
   sendMSG.addEventListener("click", GetData);
+  PrintClients();
   Request();
 }
 
@@ -13,13 +24,15 @@ function GetData() {
   const telNumber = document.querySelector(".telNumber").value;
 
   const formData = {
-    username: username,
-    email: email,
-    message: message,
-    telNumber: telNumber
+    ID: "1",
+    Name: username,
+    Email: email,
+    TelNumber: telNumber,
+    Message: message
   };
 
-  console.log("formData", formData);
+  userArr.push(formData);
+  PrintClients();
 }
 
 function Request() {
@@ -40,4 +53,39 @@ function Request() {
     }
   };
   xhr.send();
+}
+
+function PrintClients() {
+  console.log("userArr", userArr);
+
+  let printUser = document.querySelector(".printUser");
+  // printUser.removeChild("userTable");
+
+  let chkTable = document.getElementsByClassName("userTable");
+  if (chkTable.length > 0) {
+    printUser.firstChild.remove();
+  }
+
+  let table = document.createElement("table");
+  table.setAttribute("class", "userTable");
+  for (let i = 0; i < userArr.length; i++) {
+    let th = document.createElement("tr");
+    let tableID = document.createElement("td");
+    let tableName = document.createElement("td");
+    let tableEmail = document.createElement("td");
+    let tableTelNumber = document.createElement("td");
+    let tableMessage = document.createElement("td");
+    tableID.innerHTML = userArr[i].ID;
+    tableName.innerHTML = userArr[i].Name;
+    tableEmail.innerHTML = userArr[i].Email;
+    tableTelNumber.innerHTML = userArr[i].TelNumber;
+    tableMessage.innerHTML = userArr[i].Message;
+    th.appendChild(tableID);
+    th.appendChild(tableName);
+    th.appendChild(tableEmail);
+    th.appendChild(tableTelNumber);
+    th.appendChild(tableMessage);
+    table.appendChild(th);
+  }
+  printUser.appendChild(table);
 }
