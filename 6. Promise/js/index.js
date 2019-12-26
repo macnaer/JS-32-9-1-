4d85145a889d2c361744f4e7522e678dd4a6734c
master <master@rivne.itstep.org>
2019-12-26 10:02:10 +0200

    Promise continue

--- 6. Promise/js/index.js
+++ 6. Promise/js/index.js
@@ -1,18 +1,61 @@
 // const URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
 // const URL = "https://swapi.co/api/people/";
-const URL = "https://swapi.co/api/people/?page=2";
+// const URL = "https://swapi.co/api/people/?page=2";
 
-fetch(URL, {
-  method: "GET"
+// fetch(URL, {
+//   method: "GET"
+// })
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
+//     CurrencyExange(data);
+//   })
+//   .catch(err => console.log("Catch => ", err));
+
+// function CurrencyExange(data) {
+//   console.log(data);
+// }
+
+const promise = new Promise((resolve, reject) => {
+  // reject("Bad idea");
+  let car = {
+    vendor: "Tesla",
+    model: "X",
+    engineType: "Electro",
+    price: 120000
+  };
+  setTimeout(() => {
+    //reject("Bad idea");
+    resolve(car);
+  }, 3000);
 })
-  .then(response => {
-    return response.json();
-  })
   .then(data => {
-    CurrencyExange(data);
+    return new Promise((resolve, reject) => {
+      // reject("Bad document!");
+      data = {
+        vendor: "Tesla",
+        model: "X",
+        engineType: "Electro",
+        price: 120000,
+        police: "Document good"
+      };
+      resolve(data);
+    }).then(data => {
+      return new Promise((resolve, reject) => {
+        // reject("Car is broken!");
+        data = {
+          vendor: "Tesla",
+          model: "X",
+          engineType: "Electro",
+          price: 120000,
+          police: "Document good",
+          CTO: "Technical is good"
+        };
+        resolve(data);
+      }).then(data => {
+        console.log("You bought = ", data);
+      });
+    });
   })
-  .catch(err => console.log("Catch => ", err));
-
-function CurrencyExange(data) {
-  console.log(data);
-}
+  .catch(err => console.log("Error Type", err));
